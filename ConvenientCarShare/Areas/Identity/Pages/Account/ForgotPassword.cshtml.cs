fbaf0e2021f51ff8eb0c9c5d764a6cef43f3b7b0
Jessicarrr <JWP01993@gmail.com>
2025-03-22 23:13:13 +1100

    refactor: changed "forgot password" email to have different wording/layout

--- ConvenientCarShare/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ ConvenientCarShare/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -57,7 +57,7 @@ namespace ConvenientCarShare.Areas.Identity.Pages.Account
                 await _emailSender.SendEmailAsync(
                     Input.Email,
                     "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    $"Please reset your password by going to the following link:<br/><br/>{HtmlEncoder.Default.Encode(callbackUrl)}");
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
             }
