e07908e78058ff0c4591a4ab0c5fd8fa1535e6c7
Jessicarrr <JWP01993@gmail.com>
2025-03-22 22:58:13 +1100

    refactor: change deprecated code in startup, changed cookie consent needed to true

--- ConvenientCarShare/Startup.cs
+++ ConvenientCarShare/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace ConvenientCarShare
@@ -28,7 +29,7 @@ namespace ConvenientCarShare
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => false;
+                options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -60,7 +61,7 @@ namespace ConvenientCarShare
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
