3ebc2280870235d547943633bbafc5e0f8800b7f
Jessicarrr <JWP01993@gmail.com>
2025-03-26 16:02:51 +1100

    refactor: TestHelpers cleanup + booking creation
    - Cleaned up various parts of the TestHelpers file
    - Try to create unique connection for each time GetSqlInmemoryContext is called.
    - Create a "GetDummyBooking" method.

--- Test-Xunit-CCS/TestHelpers.cs
+++ Test-Xunit-CCS/TestHelpers.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Test_Xunit_CCS
@@ -19,25 +18,19 @@ namespace Test_Xunit_CCS
     public static class TestHelpers
     {
         /// <summary>
-        /// Get a reference to a fake ApplicationUser while also adding them to the
-        /// in memory database.
+        /// Get a reference to a fake ApplicationUser while also adding them to the in-memory database.
         /// </summary>
-        /// <returns>An ApplicationUser instance.</returns>
         public static ApplicationUser GetDummyUser(string id, string email, ApplicationDbContext context)
         {
             var testUser = new ApplicationUser { Id = id, Email = email };
-
             context.Users.Add(testUser);
             context.SaveChanges();
-
             return testUser;
         }
 
         /// <summary>
         /// Create a fake car in the ApplicationDbContext and save it to the database.
         /// </summary>
-        /// <param name="context">The db context currently being used.</param>
-        /// <returns></returns>
         public static Car GetDummyCar(ApplicationDbContext context)
         {
             var testCar = new Car("test-brand",
@@ -47,23 +40,48 @@ namespace Test_Xunit_CCS
                 23.0,
                 23.0,
                 50.0M,
-                "Four"
-            );
-
+                "Four");
             context.Cars.Add(testCar);
             context.SaveChanges();
-
             return testCar;
         }
 
         /// <summary>
-        /// Get an ApplicationDbContext, in this case to test db actions. This uses an in-memory
-        /// sqlite database so the actual database on disk is not affected.
+        /// Create a fake parking area and add it to the context.
         /// </summary>
-        /// <returns>An ApplicationDbContext instance.</returns>
+        public static ParkingArea GetDummyParkingArea(ApplicationDbContext context, int width = 24, int height = 12, int capacity = 5)
+        {
+            var parkingArea = new ParkingArea(width, height, capacity);
+            context.Add(parkingArea);
+            context.SaveChanges();
+            return parkingArea;
+        }
+
+        /// <summary>
+        /// Create a dummy booking in the ApplicationDbContext and save it.
+        /// </summary>
+        public static Booking GetDummyBooking(ApplicationDbContext context, ApplicationUser user, Car car, DateTime startDate, DateTime endDate, string status)
+        {
+            var booking = new Booking
+            {
+                User = user,
+                Car = car,
+                StartDate = startDate,
+                EndDate = endDate,
+                Status = status
+            };
+            context.Bookings.Add(booking);
+            context.SaveChanges();
+            return booking;
+        }
+
+        /// <summary>
+        /// Get an ApplicationDbContext for testing using an in-memory SQLite database.
+        /// </summary>
         public static ApplicationDbContext GetSqliteInMemoryContext()
         {
-            var connection = new SqliteConnection("DataSource=:memory:");
+            var connectionString = $"Data Source={Guid.NewGuid()};Mode=Memory;Cache=Shared";
+            var connection = new SqliteConnection(connectionString);
             connection.Open();
 
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -79,7 +97,6 @@ namespace Test_Xunit_CCS
         /// <summary>
         /// Create a fake identity to test tasks that require a login or a user to exist.
         /// </summary>
-        /// <returns>Returns (ClaimsIdentity, ClaimsPrinciple)</returns>
         public static (ClaimsIdentity, ClaimsPrincipal) GetClaimsIdentity()
         {
             var claims = new List<Claim>
@@ -90,42 +107,24 @@ namespace Test_Xunit_CCS
 
             var identity = new ClaimsIdentity(claims, "TestAuthType");
             var principal = new ClaimsPrincipal(identity);
-
             return (identity, principal);
         }
 
         /// <summary>
         /// Create a user manager for various services in the project using Mock.
         /// </summary>
-        /// <returns></returns>
         public static UserManager<ApplicationUser> GetUserManager()
         {
-            // Create a mock for the IUserStore
             var store = new Mock<IUserStore<ApplicationUser>>().Object;
-
-            // Use Options.Create to provide an IdentityOptions instance
             var identityOptions = Options.Create(new IdentityOptions());
-
-            // Use a PasswordHasher
             var passwordHasher = new PasswordHasher<ApplicationUser>();
-
-            // Provide empty lists for validators
             var userValidators = new List<IUserValidator<ApplicationUser>>();
             var passwordValidators = new List<IPasswordValidator<ApplicationUser>>();
-
-            // Use a real lookup normalizer
             var keyNormalizer = new UpperInvariantLookupNormalizer();
-
-            // Provide an instance of IdentityErrorDescriber
             var identityErrorDescriber = new IdentityErrorDescriber();
-
-            // Build a minimal IServiceProvider. This is optional and can be replaced with a mock if needed.
             var services = new ServiceCollection().BuildServiceProvider();
-
-            // Create a mock for ILogger<UserManager<ApplicationUser>>
             var logger = new Mock<ILogger<UserManager<ApplicationUser>>>().Object;
 
-            // After all that, we can now create a userManager.
             var userManager = new UserManager<ApplicationUser>(
                 store,
                 identityOptions,
