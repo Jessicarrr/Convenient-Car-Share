a636e1e50aec19efe288de81aa8602131ea197ed
Jessicarrr <JWP01993@gmail.com>
2025-03-27 18:32:09 +1100

    feat: HealthController: automatic up/down check
    - Add a HealthController that returns an OK result on get.
    - This is for automated checking if the website is up and running.

---
+++ ConvenientCarShare/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConvenientCarShare.Controllers
+{
+    [ApiController]
+    [Route("Health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success - things are running as expected.");
+        }
+    }
+}
